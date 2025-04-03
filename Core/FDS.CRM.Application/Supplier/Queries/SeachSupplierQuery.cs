ebb322807f1a452b1a83ede8f8d0aca1869d06b0
Văn Sỹ <vansy9x@gmail.com>
2025-04-03 14:40:50 +0700

    Commit code

---
+++ Core/FDS.CRM.Application/Supplier/Queries/SeachSupplierQuery.cs
@@ -0,0 +1,46 @@
+﻿using FDS.CRM.Application.Common.Queries;
+using FDS.CRM.Application.Contact.Queries;
+using System.Linq.Expressions;
+
+namespace FDS.CRM.Application.Supplier.Queries;
+
+public class SeachSupplierQuery : BaseSearchQuery<Domain.Entities.Supplier, SupplierDto>
+{
+    public SeachSupplierQuery(SearchSupplierQueryParams queryParams) 
+    {
+        SearchRequest = queryParams.ToSearchRequest();
+    }
+
+    public override IQueryable<Domain.Entities.Supplier> AddIncludes(IQueryable<Domain.Entities.Supplier> query)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override IOrderedQueryable<Domain.Entities.Supplier> ApplySort(IQueryable<Domain.Entities.Supplier> query, string sortField, bool isDescending)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Expression<Func<Domain.Entities.Supplier, bool>> GetFilterExpression(SearchCondition condition)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Expression<Func<Domain.Entities.Supplier, SupplierDto>> GetSelectExpression()
+    {
+        throw new NotImplementedException();
+    }
+}
+public class SearchSupplierQueryHandler : BaseSearchQueryHandler<Domain.Entities.Supplier, SupplierDto, SeachSupplierQuery>
+{
+    public SearchSupplierQueryHandler(ICrudService<Domain.Entities.Supplier> crudService) : base(crudService)
+    {
+    }
+
+    public override async Task<SearchResponseModel<SupplierDto>> HandleAsync(
+       SeachSupplierQuery query,
+       CancellationToken cancellationToken = default)
+    {
+
+    }
+}
---
+++ Core/FDS.CRM.Application/Supplier/Queries/SearchSupplierQueryParams.cs
@@ -0,0 +1,33 @@
+﻿namespace FDS.CRM.Application.Supplier.Queries;
+
+public class SearchSupplierQueryParams
+{
+    public string? Name { get; set; }
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+    public string? SortField { get; set; }
+    public bool IsDescending { get; set; }
+
+    public SearchRequestModel ToSearchRequest()
+    {
+        var conditions = new List<SearchCondition>();
+
+        if (!string.IsNullOrEmpty(Name))
+        {
+            conditions.Add(new SearchCondition
+            {
+                Field = "Name",
+                Operator = "contains",
+                Value = Name
+            });
+        }
+        return new SearchRequestModel
+        {
+            PageNumber = PageNumber,
+            PageSize = PageSize,
+            SortField = SortField,
+            IsDescending = IsDescending,
+            Conditions = conditions
+        };
+    }
+}
